4ec395ec8558ef60a91764e2e99a790b56bbbb9b
eralpkor <eralp.ny@gmail.com>
2019-08-13 19:25:22 -0400

    more css

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 
 const Display = () => {
   return <div className='display'>
-    <h1>0</h1>
+    <h1>-9</h1>
     {   /* Display any props data here */}
     </div>;
 };
